1ce6f5714bf591c0e5f5624ee6d2e6470ff97ab4
Corey DeLoach <cdeloach16@gmail.com>
2021-11-15 21:50:34 -0600

    more changes happening, using cards now for all projects

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Projects = () => {
+
     return (  
-                <div className="col-md-12 bgcolor">
+        <>
+                <div className="d-flex justify-content-center bgcolor ">
+                    <div className="card border rounded shadow">
+                        <h2 className="card-header">c^2 coffee</h2>
+                        <div className="card-body">
+                            <div className="card-text">
+                                <ul className="list-group">
+                                    <li className="list-group-item">Sole creator and engineer of this full-stack application, inspired by my love for coffee</li>
+                                    <li className="list-group-item">Clients have ability to register or login with Passport Authentication </li>
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                    <li className="list-group-item"></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="card-footer"></div>
+                    </div>
+                </div>
+                {/* <div className="col-md-12 bgcolor">
                     <div className="Project display-5">
                         inspired by my love for coffee
                     </div>
-                    <a className="link" href="http://cc-coffee.herokuapp.com" target="_blank" rel="noopener noreferrer">c^2 coffee</a>
+                    <div>
+                        <a className="link" href="http://cc-coffee.herokuapp.com" target="_blank" rel="noopener noreferrer">c^2 coffee</a>
+                    </div>
                     <div className="Project display-5">
                         more projects can be seen on my
                     </div> 
                     <a className="link" href="http://www.github.com/Slap-a-tha-Bass" target="_blank" rel="noopener noreferrer">github</a>
-                </div>
+                </div> */}
+        </>
     )
 }
 
