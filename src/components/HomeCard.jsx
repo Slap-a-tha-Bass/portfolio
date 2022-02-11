46a86c5586e2cde4ae71746570aed3ed860d9ebb
Corey DeLoach <cdeloach16@gmail.com>
2022-02-11 08:56:53 -0600

    projects button added, updated resume

--- src/components/HomeCard.jsx
+++ src/components/HomeCard.jsx
@@ -6,19 +6,29 @@ import {
   CardFooter,
   DeliverResultsCard,
   ContactButton,
-  HomeCardText
+  HomeCardText,
 } from "../styled-components";
 
-const HomeCard = ({ title, content, link, path }) => {
+const HomeCard = ({
+  title,
+  content,
+  link,
+  path,
+  projects_link,
+  projects_path,
+}) => {
   return (
     <DeliverResultsCard className="chess-img transition">
       <CardTitle>{title}</CardTitle>
       <Card.Body>
-       <HomeCardText>{content}</HomeCardText>
+        <HomeCardText>{content}</HomeCardText>
       </Card.Body>
       <CardFooter className="d-flex justify-content-center">
-        <Link className="text-decoration-none" to={path || ''}>
+        <Link className="text-decoration-none" to={path || ""}>
           <ContactButton>{link}</ContactButton>
+        </Link>
+        <Link className="text-decoration-none" to={projects_path || ""}>
+          <ContactButton>{projects_link}</ContactButton>
         </Link>
       </CardFooter>
     </DeliverResultsCard>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,11 +5,13 @@ const Home = () => {
   return (
     <>
       <div className="bg-color">
-        <HomeCard 
-        title="Hi, my name is Corey DeLoach"
-        content="I build full stack apps using React"
-        link="Contact me"
-        path="/contact"
+        <HomeCard
+          title="Hi, my name is Corey DeLoach"
+          content="I build full stack apps using React"
+          link="Contact me"
+          path="/contact"
+          projects_link="Projects"
+          projects_path="/projects"
         />
       </div>
     </>
